4291027d02ae91d1abd8af040a17cef9a3be6d07
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-10-23 20:33:38 +0200

    better test

--- tests.py
+++ tests.py
@@ -8,9 +8,9 @@ from slumber_serializers import CsvSerializer
 class CsvSerializerTestCase(unittest.TestCase):
     def setUp(self):
         self.data = [
-            [
-                'foo', 'bar'
-            ]
+            ['foo', 'bar'],
+            ['1', '2'],
+            ['3', '4'],
         ]
 
         self.serializers = slumber.serialize.Serializer(serializers=[slumber.serialize.JsonSerializer(),
@@ -27,10 +27,10 @@ class CsvSerializerTestCase(unittest.TestCase):
         serializer = self.serializers.get_serializer(content_type='text/csv')
 
         result = serializer.dumps(self.data)
-        self.assertEqual(result, b'foo,bar\r\n')
+        self.assertEqual(result, b'foo,bar\r\n1,2\r\n3,4\r\n')
 
     def test_loads(self):
         serializer = self.serializers.get_serializer(content_type='text/csv')
-        result = b'foo,bar'
+        result = b'foo,bar\r\n1,2\r\n3,4'
 
         self.assertEqual(self.data, list(serializer.loads(result)))
